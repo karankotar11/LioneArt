a04b4f5d8bd1eef31517cc3e2863e9c29169a7b3
karankotar11 <karankotar11@gmail.com>
2024-08-09 15:27:41 +0530

    Create index.js

---
+++ src/utils/play-click-sound/index.js
@@ -0,0 +1,6 @@
+export function playClickSound() {
+    const audio = new Audio('/sounds/clicksound.mp3');
+    audio.play().catch((error) => {
+      console.error("Failed to play sound:", error);
+    });
+  }
