92f1726ac758e495df3259073016415f88d4124b
karankotar11 <karankotar11@gmail.com>
2024-08-09 15:30:21 +0530

    Update index.js

--- src/utils/play-click-sound/index.js
+++ src/utils/play-click-sound/index.js
@@ -1,5 +1,5 @@
 export function playClickSound() {
-    const audio = new Audio('/sounds/clicksound.mp3');
+    const audio = new Audio('sounds/clicksound.mp3');
     audio.play().catch((error) => {
       console.error("Failed to play sound:", error);
     });
